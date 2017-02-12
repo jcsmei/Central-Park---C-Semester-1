7dd6ba4dedd61624f282c4174e206c9f41fe9a85
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:03:43 -0500

    Add project files.

---
+++ Central Park/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Central_Park
+{
+    partial class frmCentralPark
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picPark = new System.Windows.Forms.PictureBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picPark)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picPark
+            // 
+            this.picPark.Image = global::Central_Park.Properties.Resources.Central_Park;
+            this.picPark.Location = new System.Drawing.Point(42, 12);
+            this.picPark.Name = "picPark";
+            this.picPark.Size = new System.Drawing.Size(298, 180);
+            this.picPark.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPark.TabIndex = 0;
+            this.picPark.TabStop = false;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BackColor = System.Drawing.Color.Khaki;
+            this.lblMessage.Font = new System.Drawing.Font("Calibri", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.Location = new System.Drawing.Point(42, 237);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(298, 112);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Central Park\r\nLargest Park in NYC";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.Red;
+            this.btnExit.Location = new System.Drawing.Point(72, 386);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(235, 31);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "EXIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmCentralPark
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 459);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.picPark);
+            this.Name = "frmCentralPark";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picPark)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picPark;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
